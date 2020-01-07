ae8ffca5845cbbb0616c8ab5703af2a5e97f9418
zhangshouleibupt <shouleizhangbupt@163.com>
2020-01-07 21:00:08 +0800

    add viterbi

--- crf_lstm.py
+++ crf_lstm.py
@@ -15,7 +15,7 @@ class LSTMCRF(nn.Module):
         self.hidden_dim = hidden_dim
         self.embed = nn.Embedding(voc_size,hidden_dim)
         self.lstm = nn.LSTM(hidden_dim,hidden_dim,layer_num=lstm_layers
-                            bidirectional=True)
+                            bidirectional=True,batch_first=True)
         self.emission = nn.Linear(hidden_dim*2,tag_nums)
         self.transmision = nn.Linear(tag_nums*2,tag_nums+2)
     def neg_log_likehood(self,feats,tags):
@@ -27,10 +27,16 @@ class LSTMCRF(nn.Module):
         return add_score - golden_score
     def _get_add_score(self,feats):
         pass
-    def _vitebi_decode(self,feats):
-        pass
-    def _get_tag_score(self,feats,tags):
-        pass
-    def _get_lstm_feats(self):
-        pass
-
+    def _viterbi_decode(self,feats):
+        
+    def _get_tag_score(self,sentence,tags):
+        emission_matrix = self._get_lstm_feats(sentence)
+        p_score = torch.sum(torch.tensor([emission_matrix[i][tag] for i,tag in enumerate(tags)]))
+        transmission_score = torch.sum([self.transmission[tags[i],tags[i+1]] for i in range(len(tags)-1)])
+        return p_score + transmission_score
+    def _get_lstm_feats(self,sentence):
+        embed_out = self.embed(sentence).unsqueeze(1)
+        hidden = self.init_hidden()
+        lstm_out,hidden = self.lstm(embed_out,hidden)
+        emission_matrix = self.hidden_to_tag(lstm_out.squeeze())
+        return emission_matrix
